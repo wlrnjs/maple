55a645f006c98cdcb1ba8ddbb277b84cdc728ab6
wlrnjs <tjwlrnjs7336@icloud.com>
2024-11-29 22:52:58 +0900

    3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import {Route, Routes} from "react-router-dom";
 import Layout from "./Layout/Layout";
 import MainPage from "./Pages/MainPage/MainPage";
 import BasicInformation from "./Pages/BasicInformation/BasicInformation";
+import Abilities from "./Pages/Abilities/Abilities";
+import Equipment from "./Pages/Equipment/Equipment";
+import Skills from "./Pages/Skills/Skills";
+import Advanced from "./Pages/Advanced/Advanced";
 
 function App() {
 	return (
@@ -10,10 +14,10 @@ function App() {
 			<Route path="/" element={<Layout />}>
 				<Route index element={<MainPage />} />
 				<Route path="basic-info" element={<BasicInformation />} />
-				<Route path="abilities" element={<BasicInformation />} />
-				<Route path="equipment" element={<BasicInformation />} />
-				<Route path="skills" element={<BasicInformation />} />
-				<Route path="advanced-skills" element={<BasicInformation />} />
+				<Route path="abilities" element={<Abilities />} />
+				<Route path="equipment" element={<Equipment />} />
+				<Route path="skills" element={<Skills />} />
+				<Route path="advanced-skills" element={<Advanced />} />
 			</Route>
 		</Routes>
 	);
